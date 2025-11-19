bcd1a399e91fefb9f1083d88b1ab975acea84759
MalakYounis03 <ibrhr55@gmail.com>
2025-11-19 10:11:06 +0200

    init firebase

---
+++ lib/features/auth/cubit/auth_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/widgets.dart';
+
+part 'auth_state.dart';
+
+class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
+}
